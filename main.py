325e9601f70f43d0a6e1c25e5a654bc0d45ccf9f
Hao Wang <wanghao90515@163.com>
2023-11-20 10:33:14 +0100

    重新chatgpt

--- main.py
+++ main.py
@@ -14,26 +14,23 @@ os.makedirs(output_folder, exist_ok=True)
 for cif_file in cif_files:
     # 获取不带扩展名的文件名
     file_name = os.path.splitext(os.path.basename(cif_file))[0]
-    
+
     # 构造目标子文件夹路径
     destination_folder = os.path.join(output_folder, file_name)
     os.makedirs(destination_folder, exist_ok=True)
-    
+
     # 构造目标文件路径
     destination_file = os.path.join(destination_folder, os.path.basename(cif_file))
-    
-    # 使用 shutil.copy 进行复制
-    shutil.copy(cif_file, destination_file)
+
+    # 使用 shutil.copy2 进行复制，保留文件元数据
+    shutil.copy2(cif_file, destination_file)
 
 print(f"{len(cif_files)} files copied to {output_folder}.")
 
 # 获取所有的 "2.inp_films" 子文件夹
-structure_folders = [os.path.join('2.inp_films', folder) for folder in os.listdir('2.inp_films') if os.path.isdir(os.path.join('2.inp_films', folder))]
+structure_folders = [os.path.join(output_folder, folder) for folder in os.listdir(output_folder) if os.path.isdir(os.path.join(output_folder, folder))]
 
 # 遍历每个子文件夹，并在其中执行 cif2pos.py 脚本
-import os
-import subprocess
-
 for structure_folder in structure_folders:
     # 构造 cif2pos.py 脚本的路径
     script_path = os.path.abspath('cif2pos.py')
@@ -44,17 +41,11 @@ for structure_folder in structure_folders:
     # 获取子文件夹中的 CIF 文件名
     cif_file = os.path.join(structure_folder, f"{file_name}.cif")
 
-    # 启动一个进程并执行脚本
-    process = subprocess.Popen(['python', script_path, cif_file], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    # 使用 subprocess.run 简化代码
+    result = subprocess.run(['python', script_path, cif_file], capture_output=True, text=True)
 
-    # 等待进程执行完成
-    process.wait()
-
-    # 检查返回码，如果不为 0，表示执行失败
-    if process.returncode != 0:
+    if result.returncode != 0:
         print(f"Error: Script execution failed for folder {structure_folder}")
+        print("Error output:", result.stderr)
 
 print("Scripts executed in all 2.inp_films folders.")
-
-
-
